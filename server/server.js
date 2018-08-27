199127f5bc6595e0296d745e3d9dade299672166
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2018-08-27 17:00:40 -0500

    Basic chat public

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+
+// Statics
+app.use(express.static('public'))
+
+// Socket
+io.on('connection', (client) => {
+
+    console.log('Nueva conexión');
+    // Server greet new Conexion
+    io.emit('server_hello', {hello: 'hello'})
+
+    client.on('sendMessage', (message) =>{
+        client.broadcast.emit('receiveMessage', message)
+    })
+
+    client.on('disconnect', () =>{
+        console.log('Conexión perdida');
+    })
+})
+
+
+
+// Listennig
+server.listen(3000, () =>{
+    console.log('Server running on port 3000');
+})
